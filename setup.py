25e8fbc306ed79fc199c9171e37f098fd52941e5
erikvw <ew2789@gmail.com>
2019-03-11 21:36:06 -0500

    tox

--- setup.py
+++ setup.py
@@ -10,6 +10,11 @@ with open(join(dirname(__file__), 'README.rst')) as readme:
 with open(join(dirname(__file__), 'VERSION')) as f:
     VERSION = f.read()
 
+tests_require = ['django', 'django-simple-history', 'coverage']
+with open(join(dirname(abspath(__file__)), 'requirements.txt')) as f:
+    for line in f:
+        tests_require.append(line.strip())
+
 # allow setup.py to be run from any path
 os.chdir(normpath(join(abspath(__file__), os.pardir)))
 
@@ -30,7 +35,6 @@ setup(
         "arrow",
         "django-audit-fields",
         "django-revision",
-        "django-simple-history",
         "edc-device",
         "edc-sites",
         "edc-utils",
@@ -49,4 +53,6 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     python_requires=">=3.7",
+    tests_require=tests_require,
+    test_suite='runtests.main',
 )
