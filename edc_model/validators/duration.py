7b4c0d0d4d7c9e22785d9830bf380b24f658e051
erikvw <ew2789@gmail.com>
2020-02-29 10:10:17 -0600

    add back HH:MM duration validator

--- edc_model/validators/duration.py
+++ edc_model/validators/duration.py
@@ -13,3 +13,7 @@ ym_validator = RegexValidator(  # noqa
     "^([0-9]{1,3}y([0-5]?[0-9]m)?)$",
     message="Invalid format. Expected something like 3y5m, 1y0m, etc",
 )
+
+hm_validator2 = RegexValidator(  # noqa
+    "^([0-9]{1,3}:[0-5][0-9])$", message="Enter a valid time in hour:minutes format"
+)
