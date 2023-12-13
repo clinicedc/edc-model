90862e5dee34a660909be1b24fa7962ee5c09123
erikvw <ew2789@gmail.com>
2023-12-13 14:48:26 -0600

    test setup

--- runtests.py
+++ runtests.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python
 import logging
-import os
-import sys
-from os.path import abspath, dirname
+from pathlib import Path
 
-import django
-from django.conf import settings
-from django.test.runner import DiscoverRunner
-from edc_test_utils import DefaultTestSettings
+from edc_test_utils import DefaultTestSettings, func_main
 
 app_name = "edc_model"
-base_dir = dirname(abspath(__file__))
+base_dir = Path(__file__).absolute().parent
 
-DEFAULT_SETTINGS = DefaultTestSettings(
+project_settings = DefaultTestSettings(
     APP_NAME="edc_model",
     BASE_DIR=base_dir,
-    ETC_DIR=os.path.join(base_dir, app_name, "tests", "etc"),
-    KEY_PATH=os.path.join(base_dir, app_name, "tests", "etc"),
+    ETC_DIR=str(base_dir / app_name / "tests" / "etc"),
+    KEY_PATH=str(base_dir / app_name / "tests" / "etc"),
     ALLOWED_HOSTS=["localhost"],
     ROOT_URLCONF=f"{app_name}.tests.urls",
     STATIC_URL="/static/",
@@ -40,13 +35,7 @@ DEFAULT_SETTINGS = DefaultTestSettings(
 
 
 def main():
-    if not settings.configured:
-        settings.configure(**DEFAULT_SETTINGS)
-    django.setup()
-    tags = [t.split("=")[1] for t in sys.argv if t.startswith("--tag")]
-    failfast = True if [t for t in sys.argv if t == "--failfast"] else False
-    failures = DiscoverRunner(failfast=failfast, tags=tags).run_tests([f"{app_name}.tests"])
-    sys.exit(failures)
+    func_main(project_settings, *[f"{app_name}.tests"])
 
 
 if __name__ == "__main__":
