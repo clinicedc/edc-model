94e1717c422fee65e74fa3cb5a7533231db75d56
erikvw <ew2789@gmail.com>
2024-01-26 09:13:51 -0600

    move post-migrate, autodiscover and/or system checks to edc-appconfig

--- edc_model/apps.py
+++ edc_model/apps.py
@@ -2,10 +2,12 @@ import sys
 
 from django.apps import AppConfig as DjangoAppConfig
 from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
+from django.core.checks import register
 from django.core.management.color import color_style
 from django.db.backends.signals import connection_created
 from edc_utils.sqlite import activate_foreign_keys
+
+from .system_checks import check_for_edc_model
 
 style = color_style()
 
@@ -15,9 +17,8 @@ class AppConfig(DjangoAppConfig):
     verbose_name = "Edc Model"
 
     def ready(self):
+        register(check_for_edc_model)
         sys.stdout.write(f"Loading {self.verbose_name} ...\n")
         connection_created.connect(activate_foreign_keys)
         sys.stdout.write(f" * default TIME_ZONE {settings.TIME_ZONE}.\n")
-        if not settings.USE_TZ:
-            raise ImproperlyConfigured("EDC requires settings.USE_TZ = True")
         sys.stdout.write(f" Done loading {self.verbose_name}.\n")
--- edc_model/tests/tests/test_models.py
+++ edc_model/tests/tests/test_models.py
@@ -1,9 +1,6 @@
 from copy import copy
 
-from django.apps import apps as django_apps
-from django.core.exceptions import ImproperlyConfigured
 from django.test import TestCase
-from django.test.utils import override_settings
 
 from edc_model.models import UrlModelMixinNoReverseMatch
 
@@ -11,11 +8,6 @@ from ..models import BasicModel, SimpleModel
 
 
 class TestModels(TestCase):
-    @override_settings(USE_TZ=False)
-    def test_tz(self):
-        app_config = django_apps.get_app_config("edc_model")
-        self.assertRaises(ImproperlyConfigured, app_config.ready)
-
     def test_base_update_fields(self):
         """Assert update fields cannot bypass modified fields."""
         obj = BasicModel.objects.create()
